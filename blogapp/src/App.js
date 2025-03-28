b5e78c676b4188dc32b135b9e6c040fee56cea59
Yasser MOUSTAINE <moustaineyasser7272@gmail.com>
2025-03-28 21:07:21 +0000

    Résolution des conflits avec main

--- blogapp/src/App.js
+++ blogapp/src/App.js
@@ -3,10 +3,16 @@ import Home from './pages/Home';
 import Blog from './pages/Blog';
 import Post from './pages/Post';
 import About from './pages/About';
+<<<<<<< HEAD
 
+=======
+import NavbarHome from './component/NavbarHome';
+>>>>>>> 78f42ac (Résolution des conflits avec main)
 function App() {
   return (
     <BrowserRouter>
+      <NavbarHome/>
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
