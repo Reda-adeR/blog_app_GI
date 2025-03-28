511877cf81c84dee6ec42c07a679b11b5274ac3d
Yasser MOUSTAINE <moustaineyasser7272@gmail.com>
2025-03-28 18:12:27 +0000

    AJOUT

--- blogapp/src/pages/Home.js
+++ blogapp/src/pages/Home.js
@@ -1,11 +1,32 @@
 import React from 'react'
-
+import {Row,Col} from 'react-bootstrap'
+import "../assets/styles/yasser/head-nav.css"
 const Home = () => {
   return (
-    <div>HomeG
-      <div style={{ fontFamily: 'General Sans', fontWeight: '600', fontSize: '48px' }}>
-      <h2>Mon titre avec General Sans</h2>
-    </div>
+    <div>
+      <Row>
+        <Col>
+        <h3 className='head-nav'>
+          FYFRE MAGAZINE
+        </h3>
+        </Col>
+        <Col>
+          <Row>
+            <Col>
+              <h3 className='head-nav-component'>ABOUT</h3>
+            </Col>
+            <Col>
+              <h3 className='head-nav-component'>BLOG</h3>
+            </Col>
+            <Col>
+              <h3 className='head-nav-component'>HOME</h3>
+            </Col>
+            <Col>
+              <h3 className='head-nav-component'>POST</h3>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </div>
     
   )
