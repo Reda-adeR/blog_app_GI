3f42960e44af4d74b1ba80ca92b347ade9a6ba3c
freecoss <abderrahimfarahi99@gmail.com>
2025-03-28 21:18:19 +0000

    resolving conflict

--- blogapp/src/App.js
+++ blogapp/src/App.js
@@ -3,14 +3,16 @@ import Home from './pages/Home';
 import Blog from './pages/Blog';
 import Post from './pages/Post';
 import About from './pages/About';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
-        <Route path="/post" element={<Post />} /> 
+        <Route path="/post" element={<Post />} />
         <Route path="/about" element={<About />} />
+        
       </Routes>
     </BrowserRouter>
   );
